ab91a7717fe5f496d9adc7a60107dd9d4cdf5ccd
Diana Prihozhyna <dianapri0303@gmail.com>
2026-04-16 16:09:34 +0100

    fix file name

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Toaster } from 'react-hot-toast';
-import QueryProvider from '@/providers/QueryProvide';
+import QueryProvider from '@/providers/QueryProvider';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-family' });
